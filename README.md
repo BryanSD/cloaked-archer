c992e73dbc7ecc6b067dc316d6a261551d0e0e7c
John Heatherington <john.heatherington@rackspace.com>
2014-04-24 17:34:46 -0400

    Adding support for postgresql
    Refer to readme for usage

--- README.md
+++ README.md
@@ -2,3 +2,34 @@ cloaked-archer
 ==============
 
 Sample To-Do using distributed database backends
+
+PostgreSQL Setup:
+
+1) sudo apt-get install postgresql postgresql-server-dev-9.1
+2) sudo -u postgres psql postgres
+3) Change the postgres role's password (\q to quit the prompt)
+
+    \password postgres
+
+4) (Optional) Create role for app with login and create privs
+
+    create role todo_admin with login password 'password' createdb
+
+5) Set up table (run psql as desired role)
+
+    createdb tododb
+    psql -U user tododb
+    CREATE TABLE todos (
+        id SERIAL,
+        title varchar NOT NULL,
+        description text NOT NULL,
+        PRIMARY KEY (id)
+    );
+
+6) pip install -r requirements.txt
+
+7) python setup.py develop
+
+8) Run as:
+
+    python server.py postgresql tododb user password
---
+++ driver_postgresql.py
@@ -0,0 +1,57 @@
+import sys
+
+import psycopg2
+
+import base_driver
+from model import ToDo
+
+
+class PostgreSQLDriver(base_driver.BaseDriver):
+
+    def __init__(self):
+        db = sys.argv[2]#'tododb'
+        user = sys.argv[3]#'john'
+        password = sys.argv[4]#'trumpet'
+        self.conn = psycopg2.connect(
+            "dbname=%s user=%s password=%s" % (db, user, password)
+        )
+
+    def get(self, id):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "SELECT * FROM todos WHERE id = %s;",
+            (id,)
+        )
+        (row_id, title, descr) = cursor.fetchone()
+        cursor.close()
+        return ToDo(title, descr, row_id)
+
+    def get_all(self):
+        cursor = self.conn.cursor()
+        cursor.execute("SELECT id, title, description FROM todos;")
+        raw_results = cursor.fetchall()
+        print raw_results
+        result = [ToDo(title, descr, row_id) for (row_id, title, descr) in raw_results]
+        print result
+        cursor.close()
+        return result
+
+    def insert(self, todo):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "INSERT INTO todos (title, description) VALUES (%s, %s);",
+            (todo.title, todo.description)
+        )
+        self.conn.commit()
+        cursor.close()
+
+    def delete(self, id):
+        cursor = self.conn.cursor()
+        cursor.execute(
+            "DELETE FROM todos WHERE id = %s;",
+            (id,)
+        )
+        self.conn.commit()
+        result = cursor.rowcount > 0
+        cursor.close()
+        return result
